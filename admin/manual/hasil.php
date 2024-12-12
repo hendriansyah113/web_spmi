a487d0509d473481da5bf8f1635e80d3cef3505b
Hendriansyah <hendriansyah113@gmail.com>
2024-12-12 12:29:20 +0700

    hasil

--- admin/manual/hasil.php
+++ admin/manual/hasil.php
@@ -81,209 +81,6 @@ if ($row = mysqli_fetch_assoc($result_total_skor)) {
     $total_skor = intval($row['total_skor']);
 }
 
-function displayData()
-{
-    // Koneksi ke database
-    $host = 'localhost';
-    $username = 'root';
-    $password = '';
-    $database = 'web_spmi';
-
-    // Koneksi ke database
-    $conn = new mysqli($host, $username, $password, $database);
-
-    // Cek koneksi
-    if ($conn->connect_error) {
-        die("Koneksi gagal: " . $conn->connect_error);
-    }
-
-    global $tahun;
-    global $prodi;
-
-    $kolom_upload = ($prodi === 'Farmasi') ? 'upload_dokumen_farmasi' : 'upload_dokumen_ak';
-    $kolom_dokumen = ($prodi === 'Farmasi') ? 'kelengkapan_dokumen_farmasi' : 'kelengkapan_dokumen_ak';
-    $catatan = ($prodi === 'Farmasi') ? 'catatan_farmasi' : 'catatan_ak';
-
-    // Menampilkan data standar
-    $sql = "SELECT * FROM standar_audit WHERE tahun = '$tahun'";
-    $result_standar_audit = $conn->query($sql);
-
-    if ($result_standar_audit->num_rows > 0) {
-        // Tampilkan standar dalam baris pertama tabel
-        echo "<table>
-                <thead>
-                    <tr>
-                        <th>Nomor</th>
-                        <th>Uraian</th>
-                        <th>Upload Dokumen</th>
-                        <th>Kelengkapan Dokumen</th>
-                        <th>Catatan</th>
-                        <th>Aksi</th>
-                         <th>Aksi Admin</th>
-                    </tr>
-                </thead>
-                <tbody>";
-
-        // Menampilkan standar pada baris pertama
-        while ($row = $result_standar_audit->fetch_assoc()) {
-            echo "<tr>
-                    <td>" . $row['id'] . "</td>
-                    <td>" . $row['nama_standar'] . "</td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                  </tr>";
-
-            // Menampilkan soal audit terkait standar ini
-            $standar_id = $row['id'];
-            $sql_audit = "SELECT * FROM audit_dokumen WHERE standar_id = $standar_id ORDER BY soal_nomor";
-            $result_audit = $conn->query($sql_audit);
-
-            if ($result_audit->num_rows > 0) {
-                while ($audit = $result_audit->fetch_assoc()) {
-                    echo "<tr>
-                            <td>" . $audit['soal_nomor'] . "</td>
-                            <td>" . $audit['uraian'] . "</td>
-                            <td>";
-                    // Cek apakah ada dokumen yang diupload
-                    if ($audit[$kolom_upload]) {
-                        echo "<a href='uploads/" . $audit[$kolom_upload] . "' target='_blank'>Lihat Dokumen</a>";
-                    } else {
-                        echo "Tidak ada dokumen";
-                    }
-                    echo "</td>
-                             <td>" . (($audit[$kolom_dokumen] === 'Lengkap') ? '✔' : '✖') . "</td>
-                            <td>" . $audit[$catatan] . "</td>
-                            <td>
-                                <form action='upload.php' method='POST' enctype='multipart/form-data'>
-                                    <div class='mb-3'>
-                                        <input type='file' name='upload_dokumen' class='form-control' accept='.pdf, .doc, .docx, .jpg, .jpeg, .png' />
-                                        <input type='hidden' name='audit_id' value='" . $audit['id'] . "' />
-                                           <input type='hidden' name='prodi' value='" . $prodi . "' /> <!-- Menambahkan input prodi -->
-                                        <input type='submit' value='Upload' class='btn btn-primary mt-2' />
-                                    </div>
-                                </form>
-                            </td>
-                             <td>
-                <button class='btn btn-primary' data-bs-toggle='modal' data-bs-target='#modalAudit" . $audit['id'] . "'>Aksi Audit</button>
-            </td>
-                          </tr>";
-
-                    // Modal untuk audit
-                    echo "<div class='modal fade' id='modalAudit" . $audit['id'] . "' tabindex='-1' aria-labelledby='modalLabel" . $audit['id'] . "' aria-hidden='true'>
-    <div class='modal-dialog'>
-        <div class='modal-content'>
-            <div class='modal-header'>
-                <h5 class='modal-title'>Form Audit: " . $audit['uraian'] . "</h5>
-                <button type='button' class='btn-close' data-bs-dismiss='modal' aria-label='Close'></button>
-            </div>
-            <form action='audit_action.php' method='POST'>
-                <div class='modal-body'>
-                    <input type='hidden' name='audit_id' value='" . $audit['id'] . "' />
-                    <input type='hidden' name='prodi' value='" . $prodi . "' /> <!-- Hidden input untuk prodi -->
-                    <div class='mb-3'>
-                        <label for='keLan_dokumen' class='form-label'>Kelengkapan Dokumen</label>
-                        <select name='kelengkapan_dokumen' class='form-control'>
-                            <option value='Lengkap'>Lengkap</option>
-                            <option value='Tidak Lengkap'>Tidak Lengkap</option>
-                        </select>
-                    </div>
-                    <div class='mb-3'>
-                        <label for='catatan' class='form-label'>Catatan</label>
-                        <textarea name='catatan' class='form-control' rows='3'></textarea>
-                    </div>
-                </div>
-                <div class='modal-footer'>
-                    <button type='button' class='btn btn-secondary' data-bs-dismiss='modal'>Tutup</button>
-                    <button type='submit' class='btn btn-primary'>Simpan</button>
-                </div>
-            </form>
-        </div>
-    </div>
-  </div>";
-
-                    // Menampilkan soal sub audit terkait soal audit ini
-                    $audit_id = $audit['id'];
-                    $sql_sub_audit = "SELECT * FROM audit_soal WHERE audit_id = $audit_id";
-                    $result_sub_audit = $conn->query($sql_sub_audit);
-
-                    if ($result_sub_audit->num_rows > 0) {
-                        while ($sub_audit = $result_sub_audit->fetch_assoc()) {
-                            echo "<tr>
-                                    <td>" . '' . "</td>
-                                    <td>" . $sub_audit['uraian'] . "</td>
-                                    <td>";
-                            // Cek apakah ada dokumen yang diupload untuk sub audit
-                            if ($sub_audit[$kolom_upload]) {
-                                echo "<a href='uploads/" . $sub_audit[$kolom_upload] . "' target='_blank'>Lihat Dokumen</a>";
-                            } else {
-                                echo "Tidak ada dokumen";
-                            }
-                            echo "</td>
-                                    <td>" . (($sub_audit[$kolom_dokumen] === 'Lengkap') ? '✔' : '✖') . "</td>
-                                    <td>" . $sub_audit[$catatan] . "</td>
-                                    <td>
-                                        <form action='upload.php' method='POST' enctype='multipart/form-data'>
-                                            <div class='mb-3'>
-                                                <input type='file' name='upload_dokumen' class='form-control' accept='.pdf, .doc, .docx, .jpg, .jpeg, .png' />
-                                                <input type='hidden' name='sub_audit_id' value='" . $sub_audit['id'] . "' />
-                                                   <input type='hidden' name='prodi' value='" . $prodi . "' /> <!-- Menambahkan input prodi -->
-                                                <input type='submit' value='Upload' class='btn btn-primary mt-2' />
-                                            </div>
-                                        </form>
-                                    </td>
-                                    <td>
-                    <button class='btn btn-success' data-bs-toggle='modal' data-bs-target='#modalSubAudit" . $sub_audit['id'] . "'>Aksi Sub-Audit</button>
-                </td>
-                                  </tr>";
-
-                            // Modal untuk sub-audit
-                            echo "<div class='modal fade' id='modalSubAudit" . $sub_audit['id'] . "' tabindex='-1' aria-labelledby='modalLabel" . $sub_audit['id'] . "' aria-hidden='true'>
-                <div class='modal-dialog'>
-                    <div class='modal-content'>
-                        <div class='modal-header'>
-                            <h5 class='modal-title'>Form Sub-Audit: " . $sub_audit['uraian'] . "</h5>
-                            <button type='button' class='btn-close' data-bs-dismiss='modal' aria-label='Close'></button>
-                        </div>
-                        <form action='sub_audit_action.php' method='POST'>
-                            <div class='modal-body'>
-                                <input type='hidden' name='sub_audit_id' value='" . $sub_audit['id'] . "' />
-                                <input type='hidden' name='prodi' value='" . $prodi . "' /> <!-- Hidden input untuk prodi -->
-                                <div class='mb-3'>
-                                    <label for='kelengkapan_dokumen' class='form-label'>Kelengkapan Dokumen</label>
-                                    <select name='kelengkapan_dokumen' class='form-control'>
-                                        <option value='Lengkap'>Lengkap</option>
-                                        <option value='Tidak Lengkap'>Tidak Lengkap</option>
-                                    </select>
-                                </div>
-                                <div class='mb-3'>
-                                    <label for='catatan' class='form-label'>Catatan</label>
-                                    <textarea name='catatan' class='form-control' rows='3'></textarea>
-                                </div>
-                            </div>
-                            <div class='modal-footer'>
-                                <button type='button' class='btn btn-secondary' data-bs-dismiss='modal'>Tutup</button>
-                                <button type='submit' class='btn btn-primary'>Simpan</button>
-                            </div>
-                        </form>
-                    </div>
-                </div>
-              </div>";
-                        }
-                    }
-                }
-            }
-        }
-
-        echo "</tbody>
-              </table>";
-    } else {
-        echo "<p>Tidak ada data standar ditemukan.</p>";
-    }
-}
-
 mysqli_close($conn);
 ?>
 
@@ -394,11 +191,6 @@ mysqli_close($conn);
         th {
             background-color: #f2f2f2;
         }
-
-        .vertical {
-            writing-mode: vertical-rl;
-            text-orientation: mixed;
-        }
     </style>
 </head>
 
@@ -432,15 +224,9 @@ mysqli_close($conn);
                         <i class="fas fa-chart-line"></i>&nbsp; Audit Mutu Internal
                     </a>
                 </li>
-                <div class="divider"></div>
                 <li class="nav-item">
                     <a class="nav-link" href="manual/standar.php">
                         <i class="fas fa-chart-line"></i>&nbsp; Kelola Indikator
-                    </a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="manual/standar_audit.php">
-                        <i class="fas fa-chart-line"></i>&nbsp; Kelola Audit
                     </a>
                 </li>
                 <div class="divider"></div>
@@ -453,24 +239,25 @@ mysqli_close($conn);
         </div>
         <div class="content">
             <div class="dashboard-header text-center">
-                <h4>INSTRUMEN PENILAIAN</h4>
-                <h4>AUDIT MUTU INTERNAL PROGRAM STUDI</h4>
-                <h4>UNIVERSITAS MUHAMMADIYAH PALANGKARAYA</h4>
-                <h4>TAHUN AJARAN <?= $_GET['tahun'] ?></h4>
-            </div>
-            <div class="dashboard-header">
-                <h6>Nama Program Studi : <?= $prodi ?></h6>
-                <h6>Hari, Tgl Pelaksanaan :</h6>
-                <h6>Jam :</h6>
+                <h4>Hasil Audit Mutu Internal (AMI)</h4>
+                <h4>Program Studi <?= $prodi ?></h4>
+                <h4>Universitas Muhammadiyah Palangka Raya</h4>
             </div>
             <table>
                 <thead>
                     <tr>
-                        <th>Standar</th>
-                        <th>Sub Standar</th>
-                        <th>Indikator</th>
-                        <th>Skor</th>
-                        <th>Penilaian</th>
+                        <th rowspan="2">STANDAR</th>
+                        <th rowspan="2">KOMPONEN</th>
+                        <th rowspan="2">INDIKATOR</th>
+                        <th rowspan="2">SESUAI</th>
+                        <th colspan="3">KETIDAK SESUAIAN</th>
+                        <th rowspan="2">KETERANGAN/KESIMPULAN</th>
+                    </tr>
+                    <tr>
+                        <!-- Tambahkan kolom untuk baris di bawah KETIDAK SESUAIAN -->
+                        <th>Mayor</th>
+                        <th>Minor</th>
+                        <th>Observasi</th>
                     </tr>
                 </thead>
                 <tbody>
@@ -493,37 +280,27 @@ mysqli_close($conn);
                                 endif; ?>
 
                                 <!-- Menampilkan sub-standar hanya sekali per grup dengan rowspan sesuai indikator -->
-                                <td class="vertical" rowspan="<?= count($sub_standar['indikator']); ?>">
+                                <td rowspan="<?= count($sub_standar['indikator']); ?>">
                                     <?= $sub_standar['nama']; ?>
                                 </td>
 
                                 <!-- Menampilkan indikator untuk sub-standar -->
                                 <td><?= $sub_standar['indikator'][0]['nama']; ?></td>
-                                <td><?= isset($sub_standar['indikator'][0]['skor']) ? $sub_standar['indikator'][0]['skor'] : 'Tidak ada skor'; ?>
-                                </td>
-
-                                <!-- Kolom Penilaian, tombol untuk membuka modal -->
-                                <td>
-                                    <button type="button" class="btn btn-primary" data-bs-toggle="modal"
-                                        data-bs-target="#penilaianModal"
-                                        data-indikator-id="<?= $sub_standar['indikator'][0]['id']; ?>">
-                                        Penilaian
-                                    </button>
-                                </td>
+                                <td></td>
+                                <td></td>
+                                <td></td>
+                                <td></td>
+                                <td></td>
                             </tr>
                             <!-- Tampilkan indikator lainnya jika ada -->
                             <?php for ($i = 1; $i < count($sub_standar['indikator']); $i++): ?>
                                 <tr>
                                     <td><?= $sub_standar['indikator'][$i]['nama']; ?></td>
-                                    <td><?= isset($sub_standar['indikator'][$i]['skor']) ? $sub_standar['indikator'][$i]['skor'] : 'Tidak ada skor'; ?>
-                                    </td>
-
-                                    <td>
-                                        <button type="button" class="btn btn-primary" data-bs-toggle="modal"
-                                            data-bs-target="#penilaianModal"
-                                            data-indikator-id="<?= $sub_standar['indikator'][$i]['id']; ?>">
-                                            Penilaian
-                                        </button>
+                                    <td></td>
+                                    <td></td>
+                                    <td></td>
+                                    <td></td>
+                                    <td></td>
                                     </td>
                                 </tr>
                             <?php endfor; ?>
@@ -534,21 +311,13 @@ mysqli_close($conn);
                     <tr>
                         <th colspan="2">Jumlah Indikator</th>
                         <th><?= $total_indikator_tfoot; ?></th>
-                        <th colspan="2"><?= $total_skor; ?> Skor</th>
+                        <th colspan="5"></th>
                     </tr>
                 </tfoot>
 
             </table>
             <br>
-            <br>
-            <br>
-            <div class="dashboard-header text-center">
-                <h4>INSTRUMEN KELENGKAPAN DOKUMEN</h4>
-                <h4>AUDIT MUTU INTERNAL TAHUN <?= $_GET['tahun'] ?></h4>
-            </div>
-            <?php displayData(); ?>
-            <br>
-            <a href="hasil.php?tahun=<?= $tahun ?>&prodi=<?= $prodi ?>" class="btn btn-secondary">Hasil Audit</a>
+            <a href="penilaian.php?tahun=<?= $tahun ?>&prodi=<?= $prodi ?>" class="btn btn-secondary">Kembali</a>
         </div>
 
         <!-- Modal Penilaian -->
@@ -573,7 +342,6 @@ mysqli_close($conn);
             </div>
         </div>
     </div>
-
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js"></script>
     <script>
